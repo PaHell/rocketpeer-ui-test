f435ae5a045c0dc75379c9701ea69aeed8d6a04c
Marius Ihring <89946878+mariusihring@users.noreply.github.com>
2022-07-14 20:31:59 +0200

    added folderstructure for Navbar

---
+++ src/routes/components/navbar/navbar.svelte
@@ -0,0 +1,8 @@
+<script context="module">
+</script>
+
+<template>
+	<div>
+		<h2>Navbar</h2>
+	</div>
+</template>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,18 +1,17 @@
 <script context="module">
+	import Navbar from './components/navbar/navbar.svelte';
 </script>
 
 <script>
 </script>
 
 <template>
-	<h1 class="text-red-500">Welcome to SvelteKit</h1>
-	<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-	<div class="bg-red-500 outline-blue-2">
-		<p>Hello there</p>
+	<div>
+		<div>
+			<Navbar />
+		</div>
+		<h1 class="text-3xl text-red-500">Hello World</h1>
 	</div>
-	<button class="btn">Button</button>
-	<button class="btn btn-primary">Button</button>
 </template>
 
 <style lang="postcss" global>
