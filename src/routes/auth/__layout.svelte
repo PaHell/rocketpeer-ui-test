0e3c52e5514884e54fdf1c95862ff7e592f0920c
Marius Ihring <89946878+mariusihring@users.noreply.github.com>
2022-07-20 00:05:04 +0200

    removed broken and unnessecary figure from login

--- src/routes/auth/__layout.svelte
+++ src/routes/auth/__layout.svelte
@@ -5,15 +5,13 @@
 	<div class="flex-1 flex justify-center items-center mb-16">
 		<div class="w-full max-w-xs">
 			<div class="card w-96 bg-base-100 shadow-xl">
-				<figure><img src="https://placeimg.com/400/200/tech" alt="Shoes" /></figure>
 				<div class="card-body">
 					<slot />
 				</div>
-			  </div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <style lang="postcss">
-
 </style>
